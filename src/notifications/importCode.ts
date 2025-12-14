df33b5c6169e4da9865a60ddbbecfb7746d2164e
Andrew Dunn <aldunn@aeyon.us>
2025-12-14 08:05:07 -0600

    Edits

--- src/notifications/importCode.ts
+++ src/notifications/importCode.ts
@@ -2,6 +2,7 @@ import * as Notifications from "expo-notifications";
 import { ensureNotificationPermissions } from "./notifications";
 // REMOVE: import { useTranslation } from 'react-i18next';
 // REMOVE: const { t } = useTranslation();
+import { SchedulableTriggerInputTypes } from "expo-notifications"; // <--- ADD THIS
 
 // FIX: Accept title and body as arguments
 export async function notifyImportCode(title: string, body: string) {
