6ca1f3a679f775bfa201827e61c09b7af32bd286
Andrew-Unfiltered <104986765+aldunnaeyonus@users.noreply.github.com>
2025-12-25 09:08:04 -0600

    Remove API key from translate_json.sh
    Removed API key for security reasons.

--- translate_json.sh
+++ translate_json.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # --- CONFIGURATION ---
-API_KEY="AIzaSyB8tJ_aDJo55STfOty6RwAKHjo4HDxk_Is"
+API_KEY=""
 SOURCE_FILE="/Users/andrewdunn/Documents/GitHubDunn/BillBell/src/locales/en.json"
 MODEL="gemini-2.5-flash"
 LANGUAGES=("es-MX", "es-ES", "es" "de" "nl" "fr" "it" "ja" "zh-Hans" "pt-BR", "pt-PT", "zh-CN", "zh-Hant", "pt", "zh")
