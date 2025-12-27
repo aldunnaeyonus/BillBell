b452b88900502fcc5fd398155d295cb0a91dc46c
Andrew Dunn <aldunn@aeyon.us>
2025-12-27 12:19:29 -0600

    Edits

--- src/data/vendors.ts
+++ src/data/vendors.ts
@@ -15,8 +15,9 @@ export interface VendorInfo {
   categoryKey: string; // Translation key, e.g., 'category_entertainment'
 }
 
-export const bulkURL = "https://tinyurl.com/billbellapp";
-export const serverURL = "https://billbellapp.com/";
+export const bulkURL = "https://tinyurl.com/billMVP";
+//export const serverURL = "https://dunn-carabali.com/billMVP";
+export const serverURL = "https://billbellapp.com";
 
 export const normalizeVendorKey = (input: string) =>
   (input || "")
